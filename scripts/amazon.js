1395412a8e93536780f08a27778e257b427ebb08
AlexDieter <alexdieter1@gmail.com>
2024-06-13 19:59:54 -0700

    excersise problems

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart,
+  calculateCartQuantity} from '../data/cart.js';
 import {products} from'../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -62,13 +63,10 @@ document.querySelector('.js-products-grid').
 innerHTML =productsHTML;
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
+  const cartQuantity = calculateCartQuantity();
 
-  cart.forEach((cartItem) => {
-    cartQuantity+=cartItem.quantity;
-  });
-  document.querySelector('.js-cart-quantity').
-  innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity')
+  .innerHTML = cartQuantity;
 }
 function addToCartMessage(button){
   const productContainer = button.closest('.product-container');
@@ -79,6 +77,8 @@ function addToCartMessage(button){
     addMessage.classList.remove('visible');
   }, 2000);
 }
+
+updateCartQuantity();
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart,
+  calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -38,9 +39,11 @@ cart.forEach((cartItem)=> {
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link data-product-id="${matchingProduct.id}">
               Update
             </span>
+            <input class ="quantity-input">
+            <span class = "save-quantity-link link-primary">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id ="${matchingProduct.id}">
               Delete
             </span>
@@ -108,5 +111,29 @@ document.querySelectorAll('.js-delete-link')
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+
+      updateCartQuantity();
     });
   })
+
+
+function updateCartQuantity(){
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector('.js-header-quantity').
+  innerHTML = `${cartQuantity} items`;
+}
+updateCartQuantity();
+
+document.querySelectorAll('.js-update-link')
+  .forEach((link)=>{
+    link.addEventListener('click', ()=> {
+      const productId = link.dataset.productId;
+      console.log(productId);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.add('is-editing-quantity');
+    });
+  });
